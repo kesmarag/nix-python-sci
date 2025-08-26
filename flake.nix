e2adc3b9783ce7c632f6ecdac3e72e5e578bdc28
Costas Smaragdakis <kesmarag@gmail.com>
2025-08-26 20:40:26 +0300

    update

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Kesmarag's scientific Python environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      myPythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        pytorch
+        numpy
+        pandas
+        matplotlib
+        scikit-learn
+        # jupyterlab
+        pip
+      ]);
+
+    in
+    {
+      # This defines the development shell
+      devShells.${system}.default = pkgs.mkShell {
+        # The packages that will be available in the shell's PATH.
+        buildInputs = [
+          myPythonEnv
+          # Add any other system-level dependencies here if needed.
+          # pkgs.cudatoolkit
+        ];
+        shellHook = ''
+          echo "Welcome to Kesmarag's scientific Python environment!"
+          echo ""
+          python --version
+        '';
+      };
+    };
+}
